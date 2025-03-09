0a80ea77dd51c6ea686ca4f4f4fcb42554182d20
Gabr7el Ferreira <gabrielferreira.dev@icloud.com>
2025-03-09 12:37:04 -0300

    Alterações no script create_branches.sh

--- create_branches.sh
+++ create_branches.sh
@@ -10,49 +10,54 @@ fi
 current_remote=$(git remote get-url origin)
 if [[ $current_remote == https://github.com/* ]]; then
     echo "Corrigindo URL remota de HTTPS para SSH..."
-    git remote set-url origin git@github.com:ga6rielferreira/TaskBeat.git
+    git remote set-url origin git@github.com:$(git remote get-url origin | sed -E 's/https:\/\/github.com\//github.com:/')
     echo "URL remota atualizada para SSH."
 fi
 
-# Sincroniza o repositório local com o master remoto
-git checkout master
+# Sincroniza o repositório local com o master (ou main) remoto
+git checkout main 2>/dev/null || git checkout master
 if [ $? -ne 0 ]; then
-    echo "Erro: Não foi possível mudar para o branch master."
+    echo "Erro: Não foi possível mudar para o branch master ou main."
     exit 1
 fi
 
-git pull origin master
+git pull origin $(git symbolic-ref --short HEAD)
 if [ $? -ne 0 ]; then
     echo "Erro: Não foi possível sincronizar com o remoto."
     exit 1
 fi
 
-# Função para criar branch se não existir
-create_branch() {
+# Função para criar branch se não existir e sincronizar com master
+create_and_sync_branch() {
     local branch_name=$1
     if git show-ref --verify --quiet refs/heads/$branch_name; then
-        echo "Branch $branch_name já existe, pulando criação."
+        echo "Branch $branch_name já existe, sincronizando com master."
+        git checkout $branch_name
+        git merge main 2>/dev/null || git merge master  # Tenta primeiro com main e depois com master
+        if [ $? -ne 0 ]; then
+            echo "Erro ao fazer merge na branch $branch_name. Resolve conflitos, se houver."
+            exit 1
+        fi
+        git push origin $branch_name
+        echo "Branch $branch_name sincronizada com master e enviada para o remoto."
     else
-        git checkout master
+        echo "Branch $branch_name não existe, criando e enviando para o remoto."
+        git checkout main 2>/dev/null || git checkout master
         git checkout -b $branch_name
         if [ $? -ne 0 ]; then
             echo "Erro: Não foi possível criar a branch $branch_name."
             exit 1
         fi
         git push origin $branch_name
-        if [ $? -ne 0 ]; then
-            echo "Erro: Não foi possível enviar a branch $branch_name para o remoto."
-            exit 1
-        fi
         echo "Branch $branch_name criada e enviada com sucesso."
     fi
 }
 
-# Cria as branches
-create_branch feature/gabriel
-create_branch feature/hugo
-create_branch feature/andre
-create_branch feature/elvis
-create_branch feature/mirian
+# Cria e sincroniza as branches
+create_and_sync_branch feature/gabriel
+create_and_sync_branch feature/hugo
+create_and_sync_branch feature/andre
+create_and_sync_branch feature/elvis
+create_and_sync_branch feature/mirian
 
-echo "Todas as branches foram criadas ou verificadas com sucesso: feature/gabriel, feature/hugo, feature/andre, feature/elvis, feature/mirian"
+echo "Todas as branches foram criadas ou sincronizadas com sucesso."
